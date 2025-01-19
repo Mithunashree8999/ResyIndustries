87d29e37bc4d74e4fd39fc1b7203a4d18b52713a
HP <padmasree9991@gmail.com>
2025-01-19 15:00:51 +0530

    commit

--- src/app/sliderimage/sliderimage.component.ts
+++ src/app/sliderimage/sliderimage.component.ts
@@ -35,7 +35,21 @@ export class SliderimageComponent {
     'assets/images/9.jpg',
     'assets/images/10.jpg',
     'assets/images/11.jpg',
-    'assets/images/12.jpg'
+    'assets/images/12.jpg',
+    'assets/images/13.jpeg',
+    'assets/images/14.jpeg',
+    'assets/images/15.jpeg',
+    'assets/images/16.jpeg',
+    'assets/images/17.jpeg',
+    'assets/images/18.jpeg',
+    'assets/images/19.jpeg',
+    'assets/images/20.jpeg',
+    'assets/images/21.jpeg',
+    'assets/images/22.jpeg',
+    'assets/images/23.jpeg',
+    'assets/images/24.jpeg',
+    'assets/images/25.jpeg',
+    'assets/images/26.jpeg'
   ];
   currentSlide = 0;
   ngOnInit() {
